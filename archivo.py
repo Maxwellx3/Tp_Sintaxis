4bcba9acdf1642c5a0f3a23691bbdce8bb82ad2c
Maxi <maxi-93-33@hotmail.com>
2023-05-25 12:00:26 -0300

    Primer commit

---
+++ archivo.py
@@ -0,0 +1,17 @@
+def cerrarArchivo(archivo):
+    archivo.close()
+
+
+def leerCaracter(linea: str, pos: int)-> str:
+    if pos < len(linea):
+        return linea[pos]
+    else:
+        return ""       # si fin de pagina devuelve " "
+
+
+def leerLineas(archivo):
+    return archivo.readlines()
+
+
+def abrirArchivo(archivo):
+    open(archivo)
